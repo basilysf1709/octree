a13faf90536e70d2fde0c0215970e26dd476cb01
Basil Yusuf <basilyusuf1709@gmail.com>
2025-03-31 17:16:23 +0300

    export to pdf - fixed build errors

--- app/api/create-checkout-session/route.ts
+++ app/api/create-checkout-session/route.ts
@@ -5,7 +5,7 @@ import { getBaseUrl } from '@/lib/utils';
 const isDev = process.env.ENVIRONMENT === 'dev';
 
 const stripe = new Stripe(
-  isDev ? process.env.STRIPE_TEST_SECRET_KEY! : process.env.STRIPE_SECRET_KEY!, 
+  process.env.STRIPE_TEST_SECRET_KEY!, 
   { apiVersion: '2025-02-24.acacia' }
 );
 
